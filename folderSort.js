116e9f6209200a051e659b858600e3e6bbc44980
Reggie <ryoboua@gmail.com>
2019-02-10 00:33:34 -0500

    first commit

---
+++ folderSort.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+const path = require('path')
+
+function createFolder(name) {
+    return new Promise(function(resolve, reject) {
+        return fs.mkdir(path.join(__dirname, name), {}, err => {
+            if (err) {
+                if(err.code === "EEXIST") {
+                    console.log(`Folder ${name} already exists`)
+                    resolve(name)
+                } else {
+                    reject(`Failed to create folder named  ${name}`)
+                }
+            } else {
+                resolve(name)
+            }
+        } )
+    })
+}
+
+function readFiles() {
+    return new Promise(function(resolve, reject) {
+        return fs.readdir(__dirname, (err, files) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(files)
+            }
+        })
+    })
+}
+
+function moveFile(fileName, newPath) {
+    return new Promise(function(resolve, reject) {
+        return fs.rename(path.join(__dirname, fileName), path.join(newPath, fileName), err => {
+           if(err) {
+            reject(`Failed to move ${fileName}`)
+           } else {
+            resolve(console.log(`File ${fileName} succesfully moved`)) 
+           }
+        })
+    })
+}
+function reducer(acc, currentValue) { 
+        let ext = path.extname(currentValue)
+        if(ext !== '' && ext !== '.js' && !acc.includes(ext)) {
+            return acc.concat(ext)
+        } else {
+            return acc
+        }
+}
+
+async function runFolderSort() {
+    try {
+        const files = await readFiles()
+
+        if (files && files.length) {
+            const filesExtensionsInFolder = files.reduce(reducer, [])
+
+            if(filesExtensionsInFolder.length) {
+                
+                for( let ext of filesExtensionsInFolder) {
+                    ext = ext.split('.')[1]
+                    await createFolder(ext)
+                }
+    
+                for (let file of files) {
+                    let ext = path.extname(file)
+                    if(filesExtensionsInFolder.includes(ext)) {
+                        await moveFile(file, `${__dirname}/${ext.split('.')[1]}/`)
+                    }
+                }
+            } else {
+                console.log('There are no files to sort')
+            }
+        } 
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+runFolderSort()
